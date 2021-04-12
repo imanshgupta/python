6795915559d580c8553e2c571565980cabce08de
ANSH <anshgupta.mca20@gmail.com>
2021-04-13 01:54:00 +0530

    generator basic

---
+++ generators/basicgen.py
@@ -0,0 +1,17 @@
+def gen(val):
+    i = 0
+    while i < val:
+        yield i+1
+        i = i+1
+
+
+'''
+for i in gen(5):
+    print(i)
+'''
+x = gen(5)
+print(x.__next__())
+print(x.__next__())
+print(x.__next__())
+print(x.__next__())
+print(x.__next__())
